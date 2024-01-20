15a5a65f6903d1233cc68739ae77ff4bbac017cb
noobpiyush <piyush.waghela15514@sakec.ac.in>
2024-01-20 18:08:37 +0530

    climibing stairs done

--- climbingStairs.java
+++ climbingStairs.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * climbingStairs
  */
@@ -5,16 +7,53 @@ public class climbingStairs {
 
     public static void main(String[] args) {
         int n = 8;
-
-        System.out.println(climbingStairsTopDown(n));
+        int dp[] = new int[n+1];
+        Arrays.fill(dp,-1);
+        System.out.println(climbingStairsBottomUpOptimised(n));
 
     }
 
-    public static int climbingStairsTopDown(int n){
+    public static int climbingStairsTopDown(int n, int dp []){
         if(n <0) return 0;
         if(n == 0) return 1;
 
+        if(dp[n] != -1) return dp[n];
+
             // System.out.println(n);
-        return climbingStairsTopDown(n-1) + climbingStairsTopDown(n-2) + climbingStairsTopDown(n-3);
+        return dp[n] = climbingStairsTopDown(n-1,dp) + climbingStairsTopDown(n-2,dp) + climbingStairsTopDown(n-3,dp);
     }
+    public static int climbingStairsBottomUp(int n){
+       int dp[] = new int[n+1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] =2;
+
+        for(int i =3;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+
+            // System.out.println(n);
+        return dp[n];
+    }
+
+    public static int climbingStairsBottomUpOptimised(int n){
+        if(n == 0)return 1;
+        if (n<=2) {
+            return n;
+        }
+    
+       int zeroth = 1;
+       int first = 1;
+       int second = 2;
+
+       for (int i = 3; i <=n; i++) {
+        int res = zeroth + first + second;
+        zeroth = first;
+        first = second;
+        second = res;
+       }
+             // System.out.println(n);
+         return second;
+     }
 }
